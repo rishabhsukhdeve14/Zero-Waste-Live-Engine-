cd72ea1030115e915853ca457afe43ebc91d40e1
rishabhsukhdeve14 <rishabhsukhdeve14@gmail.com>
2026-05-11 23:18:01 +0530

    Update earth_engine.py

--- earth_engine.py
+++ earth_engine.py
@@ -1,46 +1,22 @@
-import ee
 import pandas as pd
 from datetime import datetime
 
-ee.Initialize(project='YOUR_PROJECT_ID')
-
-# master landfill file
 df = pd.read_csv("master_landfills.csv")
-
-dataset = ee.ImageCollection("COPERNICUS/S5P/OFFL/L3_CH4") \
-    .select("CH4_column_volume_mixing_ratio_dry_air") \
-    .sort("system:time_start", False)
-
-latest = dataset.first()
 
 results = []
 
-for index, row in df.iterrows():
+for index, row in df.head(100).iterrows():
 
-    point = ee.Geometry.Point([row['lon'], row['lat']])
-
-    value = latest.reduceRegion(
-        reducer=ee.Reducer.mean(),
-        geometry=point,
-        scale=1000
-    ).get("CH4_column_volume_mixing_ratio_dry_air")
-
-    try:
-        methane = ee.Number(value).getInfo()
-
-        results.append([
-            datetime.utcnow(),
-            row['id'],
-            row['state'],
-            row['city'],
-            row['lat'],
-            row['lon'],
-            methane,
-            "Sentinel-5P"
-        ])
-
-    except:
-        pass
+    results.append([
+        datetime.utcnow(),
+        row['id'],
+        row['state'],
+        row['city'],
+        row['lat'],
+        row['lon'],
+        1900 + index,
+        "Simulated-Live"
+    ])
 
 out = pd.DataFrame(results)
 
