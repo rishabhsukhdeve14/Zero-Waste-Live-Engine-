523ac0378bb48cf984e453a1d2a662119fe7884d
rishabhsukhdeve14 <rishabhsukhdeve14@gmail.com>
2026-05-11 23:35:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,27 @@ import pandas as pd
 
 st.title("Zero Waste Live Engine")
 
-df = pd.read_csv("live_methane_data.csv", header=None)
+st.subheader("Live Landfill Monitoring")
 
-st.write("Live Landfill Monitoring")
+df = pd.read_csv(
+    "live_methane_data.csv",
+    header=None,
+    names=[
+        "Timestamp",
+        "Landfill ID",
+        "State",
+        "City",
+        "Latitude",
+        "Longitude",
+        "Methane",
+        "Source"
+    ]
+)
 
 st.dataframe(df)
+
+st.subheader("Top Methane Sites")
+
+top_sites = df.sort_values(by="Methane", ascending=False)
+
+st.dataframe(top_sites.head(20))
