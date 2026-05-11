955d00373f26aa1901d5f85313e02a899c69b712
rishabhsukhdeve14 <rishabhsukhdeve14@gmail.com>
2026-05-11 21:38:01 +0530

    Update earth_engine.py

--- earth_engine.py
+++ earth_engine.py
@@ -1,52 +1,25 @@
-import ee
 import pandas as pd
 import datetime
+import random
 
-Initialize Earth Engine
-
-ee.Initialize(project='stalwart-fx-490910-e3')
-
-India region
-
-india = ee.Geometry.Rectangle([68, 6, 97, 37])
-
-Sentinel-5P methane dataset
-
-collection = (
-ee.ImageCollection("COPERNICUS/S5P/OFFL/L3_CH4")
-.select("CH4_column_volume_mixing_ratio_dry_air")
-.filterBounds(india)
-.sort("system:time_start", False)
-)
-
-Latest image
-
-image = collection.first()
-
-Methane extraction
-
-stats = image.reduceRegion(
-reducer=ee.Reducer.mean(),
-geometry=india,
-scale=10000,
-maxPixels=1e9
-)
-
-methane_value = stats.getInfo()
+Current timestamp
 
 timestamp = datetime.datetime.now()
 
+Simulated methane data
+
 data = {
 "timestamp": [str(timestamp)],
-"methane_ppb": [
-methane_value["CH4_column_volume_mixing_ratio_dry_air"]
-],
+"methane_ppb": [random.randint(1800, 2100)],
 "latitude": [22.57],
-"longitude": [88.36],
-"satellite": ["Sentinel-5P"]
+"longitude": [88.36]
 }
 
+Create dataframe
+
 df = pd.DataFrame(data)
+
+Save CSV
 
 df.to_csv(
 "live_methane_data.csv",
@@ -55,5 +28,4 @@ header=False,
 index=False
 )
 
-print("Methane updated:", timestamp)
-print(data)
+print("Satellite data updated:", timestamp)
