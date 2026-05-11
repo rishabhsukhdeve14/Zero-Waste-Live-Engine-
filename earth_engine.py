5b5f5909a976938080784da9e3815b546b58195a
rishabhsukhdeve14 <rishabhsukhdeve14@gmail.com>
2026-05-11 22:43:58 +0530

    Update earth_engine.py

--- earth_engine.py
+++ earth_engine.py
@@ -2,22 +2,38 @@ import pandas as pd
 import datetime
 import random
 
-timestamp = datetime.datetime.now()
+# Read landfill database
+landfills = pd.read_csv("landfills.csv")
 
-data = {
-    "timestamp": [str(timestamp)],
-    "landfill_id": ["IND_001"],
-    "state": ["West Bengal"],
-    "city": ["Kolkata"],
-    "latitude": [22.57],
-    "longitude": [88.36],
-    "methane_ppb": [random.randint(1850, 2100)],
-    "satellite": ["Sentinel-5P"],
-    "risk_score": [random.randint(1, 10)]
-}
+all_data = []
 
-df = pd.DataFrame(data)
+# Loop through every landfill
+for index, row in landfills.iterrows():
 
+    timestamp = datetime.datetime.now()
+
+    methane = random.randint(1850, 2100)
+
+    risk_score = random.randint(1, 10)
+
+    data = {
+        "timestamp": str(timestamp),
+        "landfill_id": row["landfill_id"],
+        "state": row["state"],
+        "city": row["city"],
+        "latitude": row["latitude"],
+        "longitude": row["longitude"],
+        "methane_ppb": methane,
+        "satellite": "Sentinel-5P",
+        "risk_score": risk_score
+    }
+
+    all_data.append(data)
+
+# Convert to dataframe
+df = pd.DataFrame(all_data)
+
+# Save to CSV
 df.to_csv(
     "live_methane_data.csv",
     mode="a",
@@ -25,5 +41,5 @@ df.to_csv(
     index=False
 )
 
-print("ADVANCED DATA UPDATED")
-print(data)
+print("MULTI-LANDFILL DATA UPDATED")
+print(df)
